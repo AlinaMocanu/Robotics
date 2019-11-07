a675afa1cea03c626fd488dc33e22eec525d9a20
AlinaMocanu <48868328+AlinaMocanu@users.noreply.github.com>
2019-11-07 09:04:56 +0200

    Update AnalogRGBControl.ino

--- Laboratory homeworks/AnalogRGBControl.ino
+++ Laboratory homeworks/AnalogRGBControl.ino
@@ -2,7 +2,6 @@ const int potPin0 = A0;
 const int potPin1 = A1;
 const int potPin2 = A2;
 
-
 const int redPin = 11;
 const int greenPin = 10;
 const int bluePin = 9;
@@ -15,7 +14,6 @@ int ledValue0 = 0;
 int ledValue1 = 0;
 int ledValue2 = 0;
 
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -26,7 +24,6 @@ void setup() {
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
-
 }
 
 void loop() {
@@ -40,9 +37,8 @@ void loop() {
   ledValue2 = map(potValue2, 0, 1023, 0, 255);
   
   ledControl(ledValue0, ledValue1, ledValue2);
-  
-  
 }
+
 void ledControl(int ledValue0, int ledValue1, int ledValue2)
 {
   analogWrite(redPin, ledValue0);
